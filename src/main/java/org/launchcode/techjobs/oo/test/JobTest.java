43306d912ee07a8a603ba837627e6dd0bae71d99
Andy <Afisch2014@gmail.com>
2023-02-10 00:19:20 -0800

    Just doing some initial coding. Don't know what a hashcode is. not sure if I'm missing something. Just kinda rushing the code atm.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,34 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import javax.naming.Name;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId(){
+    assertNotEquals(new Job(), new Job());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+      Job myJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(Job.getName(), "Product tester");
+        assertEquals(myJofgrb.getEmployer().getValue(), "ACME");
+        assertEquals(myJob.getLocation().getValue(), "Desert");
+        assertEquals(myJob.getPositionType().getValue(), "Quality control");
+        assertEquals(myJob.getCoreCompetency().getValue(), "Persistence");
+        assertTrue(myJob.getEmployer() instanceof Employer);
+
+
+    }
 
 }
